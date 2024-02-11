b9192154c73837c92f03c6195dd5dac0b1eb37b6
Arli Machado <arli.machado@gmail.com>
2024-02-11 00:35:40 -0300

    efetuando altercoes

--- crud-angular/src/app/app.component.ts
+++ crud-angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -9,7 +9,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, HttpClientModule],
+  imports: [RouterOutlet, MatToolbarModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
